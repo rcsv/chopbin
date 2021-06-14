0a4761631378ee1b921ed4b2c60fbce468bfc9e5
rcsv <rcsv@users.noreply.github.com>
2021-06-14 21:53:24 +0900

    Create chopbin.cmd

---
+++ chopbin.cmd
@@ -0,0 +1,25 @@
+@echo off
+1234567890123456789012345678901234567890123456789012345678901234567890123456
+REM ----------------------------------------------------------------------
+REM CHOP BINARY FILE FOR WINDOWS
+REM @author rcsvpg rcsvpg@outlook.com
+REM
+REM How to Use
+REM chopbin.cmd <filename> [filesize]
+REM <filename>
+REM [filesize] ::= 4096000
+REM ----------------------------------------------------------------------
+
+setlocal
+if not defined separate_size (
+    set /a separate_size=4096000
+)
+
+set ddf=%TEMP%\cabprof.ddf
+(echo %1) > "%ddf%"
+
+@echo on
+
+echo get start chop files
+MAKECAB /d MaxDiskSize=%separate_size% /d RptFileName=NUL /d DiskDirectoryTemplate="%~n1" /f "%ddf%"
+echo DONE
